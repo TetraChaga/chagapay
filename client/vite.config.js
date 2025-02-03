7f3e9cf890ee8c3b16f2a1ec010d61f6394b33cc
brendenozie <brendenozie@gmail.com>
2025-02-03 14:47:44 +0300

    fix: update output directory in Vite configuration to 'client/dist'

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'dist'  // Make sure this is correctly set
+    outDir: 'client/dist'  // Make sure this is correctly set
   },
   plugins: [react()],
 });
